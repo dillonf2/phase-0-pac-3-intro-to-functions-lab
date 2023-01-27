3e4d05764a512e601437c7ca6f409754de50bd15
Frank Dillon <dillonf2@outlook.com>
2023-01-26 20:05:08 -0500

    completed lab

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string){
+    // to do
+    return string.toUpperCase();
+}
+function whisper(string){
+    return string.toLowerCase()
+}
+function logShout(string){
+    console.log(shout(string))
+}
+function logWhisper(string){
+    console.log(whisper(string))
+}
+let string="hello"
+
+function sayHiToHeadphonedRoommate(string){
+   
+ if (string === string.toLowerCase()){
+    return "I can't hear you!"
+  }
+  if (string === string.toUpperCase()){
+    return "YES INDEED!"
+  }
+  if (string === "Let's have dinner together!")
+  return "I would love to!"
+} 
+
+sayHiToHeadphonedRoommate("hello")
